072d609bd365678ddcbfe80e0ded7c4740d7821e
osaidali <osaidraza2008>
2024-07-25 14:29:43 +0500

    product app created

---
+++ src/app/myinfo.tsx
@@ -0,0 +1,17 @@
+export default function Myinfo() {
+    return (
+        <>
+            <table style={{ border: '1px solid black' }}>
+                <thead>
+                    <tr>
+                        <th style={{ border: '1px solid black', padding: "10px" }}>Osaid ali</th>
+                        <th style={{ border: '1px solid black', padding: "10px" }}>Muhammed Ali</th>
+                        <th style={{ border: '1px solid black', padding: "10px" }}>228229</th>
+                    </tr>
+                </thead>
+            </table>
+        </>
+    )
+}
+
+
---
+++ src/app/product.tsx
@@ -0,0 +1,24 @@
+/*
+sr
+product name
+price
+category
+
+*/
+
+
+
+
+export default function Product() {
+    return (
+        <>
+            <tr>
+                <td style={{ border: '1px solid black', padding: "10px" }}>1</td>
+                <td style={{ border: '1px solid black', padding: "10px" }}>Iphone 15 pro max</td>
+                <td style={{ border: '1px solid black', padding: "10px" }}>225000</td>
+                <td style={{ border: '1px solid black', padding: "10px" }}>Mobile Phones</td>
+            </tr>
+        </>
+    )
+}
+
